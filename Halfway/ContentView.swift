aad95d4a4adadaca9a7accb16d01bb8e578c5f4a
lindacnatt <linda.cnattingius@gmail.com>
2020-10-07 09:43:12 +0200

    Initial Commit

---
+++ Halfway/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Halfway
+//
+//  Created by Linda Cnattingius on 2020-10-07.
+//  Copyright © 2020 Halfway. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HalfwayTests/HalfwayTests.swift
@@ -0,0 +1,34 @@
+//
+//  HalfwayTests.swift
+//  HalfwayTests
+//
+//  Created by Linda Cnattingius on 2020-10-07.
+//  Copyright © 2020 Halfway. All rights reserved.
+//
+
+import XCTest
+@testable import Halfway
+
+class HalfwayTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
