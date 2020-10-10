20cac40988ecd49439e8a64d7ab6c9e0d353e0a1
lindacnatt <linda.cnattingius@gmail.com>
2020-10-10 22:41:36 +0200

    less code of share sheet

--- Halfway/CreateInviteView.swift
+++ Halfway/CreateInviteView.swift
@@ -20,51 +20,53 @@ struct CreateInviteView: View {
                     .fontWeight(.bold)
                     .foregroundColor(Color.orange)
                 Text("Facilitating encounters")
-                
             }
             Spacer()
-            
-            Button(action: {self.showShareSheet = true}) {
+            Button(action: shareSheet) {
                 Text("Create Invite Link")
                     .font(.headline)
                     .foregroundColor(.white)
                     .padding(.horizontal, 90)
                     .padding()
-                
             }
             .background(Color.orange)
             .cornerRadius(.infinity)
             .padding(.bottom)
-            
         }
             .padding(.bottom)
-            .sheet(isPresented: $showShareSheet) {
-                ShareSheet(activityItems: ["Invitation Link"])
-            }
+//            .sheet(isPresented: $showShareSheet) {
+//                ShareSheet(activityItems: ["Invitation Link"])
+//            }
     }
+    func shareSheet(){
+        guard let link = URL(string: "https://www.apple.com") else { return }
+        let av = UIActivityViewController(activityItems: [link], applicationActivities: nil)
+        UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
+    }
+    
 }
 
-struct ShareSheet: UIViewControllerRepresentable {
-    typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
-    
-    let activityItems: [Any]
-    let applicationActivities: [UIActivity]? = nil
-    let excludedActivityTypes: [UIActivity.ActivityType]? = nil
-    let callback: Callback? = nil
-    
-    func makeUIViewController(context: Context) -> UIActivityViewController {
-        let controller = UIActivityViewController(
-            activityItems: activityItems,
-            applicationActivities: applicationActivities)
-        controller.excludedActivityTypes = excludedActivityTypes
-        controller.completionWithItemsHandler = callback
-        return controller
-    }
-    
-    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
-        // nothing to do here
-    }
-}
+//struct ShareSheet: UIViewControllerRepresentable {
+//    typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
+//
+//    let activityItems: [Any]
+//    let applicationActivities: [UIActivity]? = nil
+//    let excludedActivityTypes: [UIActivity.ActivityType]? = nil
+//    let callback: Callback? = nil
+//
+//    func makeUIViewController(context: Context) -> UIActivityViewController {
+//        let controller = UIActivityViewController(
+//            activityItems: activityItems,
+//            applicationActivities: applicationActivities)
+//        controller.excludedActivityTypes = excludedActivityTypes
+//        controller.completionWithItemsHandler = callback
+//        return controller
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+//        // nothing to do here
+//    }
+//}
 
 
 
