f8bf286b5fb4a604142599995152512adcd22ef7
lindacnatt <linda.cnattingius@gmail.com>
2020-10-10 21:40:15 +0200

    add (full) share sheet , no functionality yet

--- Halfway/CreateInviteView.swift
+++ Halfway/CreateInviteView.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct CreateInviteView: View {
+    @State private var showShareSheet = false
+    
     var body: some View {
         VStack{
             Spacer()
@@ -20,24 +22,51 @@ struct CreateInviteView: View {
                 Text("Facilitating encounters")
                 
             }
-            .padding(.bottom,200)
+            Spacer()
             
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-            Text("Create Invite Link")
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding(.horizontal, 90)
-                .padding()
+            Button(action: {self.showShareSheet = true}) {
+                Text("Create Invite Link")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 90)
+                    .padding()
                 
             }
             .background(Color.orange)
             .cornerRadius(.infinity)
             .padding(.bottom)
-        }.padding(.bottom)
-    
-        
+            
+        }
+            .padding(.bottom)
+            .sheet(isPresented: $showShareSheet) {
+                ShareSheet(activityItems: ["Invitation Link"])
+            }
     }
 }
+
+struct ShareSheet: UIViewControllerRepresentable {
+    typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
+    
+    let activityItems: [Any]
+    let applicationActivities: [UIActivity]? = nil
+    let excludedActivityTypes: [UIActivity.ActivityType]? = nil
+    let callback: Callback? = nil
+    
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: applicationActivities)
+        controller.excludedActivityTypes = excludedActivityTypes
+        controller.completionWithItemsHandler = callback
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+        // nothing to do here
+    }
+}
+
+
 
 struct CreateInviteView_Previews: PreviewProvider {
     static var previews: some View {
