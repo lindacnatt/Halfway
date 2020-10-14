d42d762f87c4ca16444ae5c85a62805512d45a8c
lindacnatt <linda.cnattingius@gmail.com>
2020-10-14 13:51:17 +0200

    testing datepicker

--- Halfway/Page Views/ContentView.swift
+++ Halfway/Page Views/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Wowzaa")
+        DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
     }
 }
 
