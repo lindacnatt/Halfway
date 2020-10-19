2f51eb61557063f65266f8d274ea55e283f925c7
mart <hans.martin.nilsson@gmail.com>
2020-10-19 15:38:02 +0200

    Added Dynamic Links capabilities

---
+++ Halfway/Page Views/TestView.swift
@@ -0,0 +1,21 @@
+//
+//  TestView.swift
+//  Halfway
+//
+//  Created by Linda Cnattingius on 2020-10-19.
+//  Copyright © 2020 Halfway. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
