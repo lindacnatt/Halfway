299e4e6524913c874acd8022f88de8477384cc42
lindacnatt <linda.cnattingius@gmail.com>
2020-10-26 18:26:41 +0100

    clean up code and change textfields

--- Halfway/Page Views/UserProfileView.swift
+++ Halfway/Page Views/UserProfileView.swift
@@ -11,47 +11,46 @@ import SwiftUI
 struct UserProfileView: View {
     
     @State private var image: Image?
-    @State var username: String = ""
+    
     
     @State private var showImagePicker = false
     @State private var inputImage: UIImage?
     
     
     var body: some View {
-        VStack (){
-            ZStack {
-                Circle()
-                    .fill(Color.gray)
-                    .shadow(color: Color.black.opacity(0.15), radius: 20, x: 5, y: 20)
-                    .overlay(Circle()
-                        .stroke(Color.blue, lineWidth: 6))
-                if image != nil {
-                    image?
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .clipShape(Circle())
+        
+        VStack{
+            VStack {
+                ZStack {
+                    Circle()
+                        .fill(Color.gray)
+                        .shadow(color: Color.black.opacity(0.15), radius: 20, x: 5, y: 20)
                         .overlay(Circle()
-                        .stroke(Color.blue, lineWidth: 6))
-                } else {
-                    Text("Select image")
+                            .stroke(Color.blue, lineWidth: 6))
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .clipShape(Circle())
+                            .overlay(Circle()
+                                .stroke(Color.blue, lineWidth: 6))
+                    } else {
+                        Image(systemName: "person").resizable().frame(width: 50, height: 50).foregroundColor(Color.white)
+                    }
                 }
+                .frame(width: 200, height: 200).padding()
+                Text("Select image").foregroundColor(Color.blue)
                 
-            }
-            .frame(width: 200, height: 200).padding()
-            .onTapGesture {
+            }.onTapGesture {
                 self.showImagePicker = true
             }
-            Text(" \(username)").font(.headline)
-            Divider()
-            TextField("Enter firstname", text: $username)
-            Divider()
-            TextField("Enter username", text: $username)
-            Divider()
+            NameFields()
             Spacer()
             
         }.sheet(isPresented: $showImagePicker, onDismiss: loadImage) {
             ImagePicker(image: self.$inputImage)
         }.padding()
+        
     }
     
     
@@ -64,5 +63,27 @@ struct UserProfileView: View {
 struct UserProfileView_Previews: PreviewProvider {
     static var previews: some View {
         UserProfileView()
+    }
+}
+
+struct NameFields: View{
+    @State var firstName: String = ""
+    @State var lastName: String = ""
+    var body: some View {
+        VStack {
+            Text("  \(firstName) \(lastName)").font(.headline).padding()
+            Divider()
+            HStack{
+                Text("Firstname").padding(.trailing)
+                TextField("Enter firstname", text: $firstName)
+            }
+            Divider()
+            HStack{
+                Text("Lastname").padding(.trailing)
+                TextField("Enter lastname", text: $lastName)
+            }
+            Divider()
+        }
+        
     }
 }
