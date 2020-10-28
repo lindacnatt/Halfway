e0251e8748366bc54d84bd9cc6992f827ac650e5
mart <hans.martin.nilsson@gmail.com>
2020-10-28 16:42:30 +0100

    Started writing functions to handle dynamic links

--- Halfway/ViewModels/CreateInviteViewModel.swift
+++ Halfway/ViewModels/CreateInviteViewModel.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SwiftUI
+import Firebase
 
 class CreateInviteViewModel {
     
@@ -17,8 +18,22 @@ class CreateInviteViewModel {
         
         let av = UIActivityViewController(activityItems: [sessionlink], applicationActivities: nil)
         UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
+        
+        guard let shareLink = DynamicLinkComponents.init(link: sessionlink, domainURIPrefix: "https://halfwayapplication.page.link") else {
+            print("Couldn't create FDL components")
+            return
+        }
+        if let myBundleID = Bundle.main.bundleIdentifier {
+        shareLink.iOSParameters = DynamicLinkIOSParameters(bundleID: myBundleID)
+        }
+        shareLink.iOSParameters?.appStoreID = "962194608" //Change later
+        shareLink.socialMetaTagParameters = DynamicLinkSocialMetaTagParameters()
+        shareLink.socialMetaTagParameters?.title = "Halfway"
+        shareLink.socialMetaTagParameters?.descriptionText = "Meet your friends half-way with the Halfway app!"
+        shareLink.socialMetaTagParameters?.imageURL = URL(string: "https://images.unsplash.com/photo-1473216635433-38f7100ae658?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2148&q=80")
+
+        
     }
-    
 }
 
 
