b4f25f77fb42c69e76ca8a2a67976657046e6d17
lindacnatt <linda.cnattingius@gmail.com>
2020-12-18 10:18:04 +0100

    remove animation that does not work

--- Halfway/Map/AnnotationView.swift
+++ Halfway/Map/AnnotationView.swift
@@ -19,7 +19,6 @@ struct AnnotationView: View {
     
     var body: some View {
         VStack{
-            //Circle().fill(Color.blue).opacity(0.8).frame(width: 50, height: 50)
             VStack{
                 Spacer()
                 Group{
@@ -31,17 +30,10 @@ struct AnnotationView: View {
                     CircleImage(image: image, width: 80, height: 80, strokeColor: strokeColor)
                     
                 }.offset(y: 8)
-                
-                Triangle()
-                    .fill(strokeColor)
+                Triangle().fill(strokeColor)
                     .frame(width: 25, height: 25)
                     .rotationEffect(.degrees(180))
-                ZStack{
-                    Circle().fill(Color.blue).opacity(self.pulsate ? 0 : 0.5).frame(width: 80, height: 80).scaleEffect(self.pulsate ? 1 : 0)
                     Circle().fill(strokeColor).frame(width: 20, height: 20)
-                }.onAppear(){
-                    self.pulsate.toggle()
-                }.animation(Animation.linear(duration: 1.7).repeatForever(autoreverses: false))
             }.frame(maxHeight: .infinity, alignment: .center).shadow(color: Color.black.opacity(0.15), radius: 8, x: 5, y: 5)
             
             Spacer()
