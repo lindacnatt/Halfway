523cd9e6344d4a402387169758be2fa5ec43f3d4
lindacnatt <linda.cnattingius@gmail.com>
2020-12-18 09:41:16 +0100

    add animation to annotation view

--- Halfway/Map/AnnotationView.swift
+++ Halfway/Map/AnnotationView.swift
@@ -15,8 +15,11 @@ struct AnnotationView: View {
     var userName: String
     var timeLeft: String
     
+    @State var pulsate = false
+    
     var body: some View {
         VStack{
+            //Circle().fill(Color.blue).opacity(0.8).frame(width: 50, height: 50)
             VStack{
                 Spacer()
                 Group{
@@ -33,8 +36,12 @@ struct AnnotationView: View {
                     .fill(strokeColor)
                     .frame(width: 25, height: 25)
                     .rotationEffect(.degrees(180))
-                Circle().fill(strokeColor).frame(width: 20, height: 20)
-                    
+                ZStack{
+                    Circle().fill(Color.blue).opacity(self.pulsate ? 0 : 0.5).frame(width: 80, height: 80).scaleEffect(self.pulsate ? 1 : 0)
+                    Circle().fill(strokeColor).frame(width: 20, height: 20)
+                }.onAppear(){
+                    self.pulsate.toggle()
+                }.animation(Animation.linear(duration: 1.7).repeatForever(autoreverses: false))
             }.frame(maxHeight: .infinity, alignment: .center).shadow(color: Color.black.opacity(0.15), radius: 8, x: 5, y: 5)
             
             Spacer()
